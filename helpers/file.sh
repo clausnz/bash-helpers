32825e67a0d924f8e86421958f37dbbda6a4eea9
Claus Bayer <claus.bayer@gmail.com>
2019-02-04 20:46:46 +1300

    Update file.sh

--- helpers/file.sh
+++ helpers/file.sh
@@ -5,7 +5,7 @@
      local file=$2
      if [ ${#line} -gt 0 ] && [ ${#file} -gt 0 ]
      then
-         sudo /bin/sh -c "echo $line >> $file";echo added "$line to file \"$file\""
+         sudo /bin/sh -c "echo $line >> $file";echo "added \"$line\" to file \"$file\""
      else
          echo "error using function \"add_to_file\""
      fi
@@ -16,7 +16,7 @@
      local file=$2
      if [ ${#search} -gt 0 ] && [ ${#file} -gt 0 ]
      then
-         sudo /bin/sh -c "sed -i '' '/$search/d' $file";echo removed line with "$search from file \"file\""
+         sudo /bin/sh -c "sed -i '' '/$search/d' $file";echo "removed line with \"$search\"" from file \"file\""
      else
          echo "error using function \"remove_from_file\""
      fi
